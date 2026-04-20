0c4453575686b5993f5109c3f4de1168a27d0e17
Mohamed <hamzatricks@gmail.com>
2026-04-20 01:25:05 +0100

    sss

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ const PORT = process.env.PORT || 3001;
 // Connect to database
 connectDB();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://babybloom-bouza.vercel.app']);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
