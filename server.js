265e1756f95863aca9e1adfa5fabe399cba30b71
Mohamed <hamzatricks@gmail.com>
2026-04-20 01:25:52 +0100

    sss

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ connectDB();
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://babybloom-bouza.vercel.app']);
+  const origin = req.headers.origin;
+  const allowedOrigins = ['http://localhost:3000', 'https://babybloom-bouza.vercel.app'];
+  
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
